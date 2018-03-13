40021533f663b70614677ab914bd7b3a261dfd0b
whesley.barnard <whesley.barnard@entelect.co.za>
2018-03-13 08:31:28 +0200

    added new idea code

---
+++ pin-mouse-app/src/components/IdeasContainer.js
@@ -0,0 +1,32 @@
+import React, {
+  Component
+} from 'react'
+import axios from 'axios'
+
+class IdeasContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/v1/ideas.json')
+    .then(response => {
+      console.log(response)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+      Ideas
+      </div>
+    )
+  }
+}
+
+export default IdeasContainer
